af07930e9656eeeb53bf524995feb7b00d411357
freddyskull <freddyskull11@gmail.com>
2026-01-31 14:18:28 -0400

    configurando para usar monorepo

--- apps/web-store/next.config.ts
+++ apps/web-store/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
